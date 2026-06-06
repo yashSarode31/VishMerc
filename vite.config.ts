92348e265cc60789e31b1facf5968bfa87d19e89
yashSarode31 <114423228+yashSarode31@users.noreply.github.com>
2026-06-06 09:31:51 +0530

    fix: explicit nitro vercel preset

--- vite.config.ts
+++ vite.config.ts
@@ -14,11 +14,26 @@ export default defineConfig({
   },
 });
 */
-import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+/*import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   nitro: { preset: "vercel" },
   tanstackStart: {
     server: { entry: "server" },
   },
+});*/
+
+import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import { nitro } from "nitro/vite";
+
+export default defineConfig({
+  nitro: { preset: "vercel" },
+  tanstackStart: {
+    server: { entry: "server" },
+  },
+  vite: {
+    plugins: [
+      nitro({ preset: "vercel" })
+    ]
+  }
 });
